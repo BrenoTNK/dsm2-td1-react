b3ffb367316d3d5e8c7abff8138a1084d3339b9b
BrenoTNK <70177109+BrenoTNK@users.noreply.github.com>
2022-11-11 22:18:11 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import Card from "./components/Card";
+
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Projeto React Redux</h1>
+
+            <div className="linha">
+                <Card />
+            </div>
+
+            <div className="linha">
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
+}
+
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import "./Card.css";
+import React from "react";
+
+
+function Card(props) {
+    return (
+        <div>
+            Card
+        </div>
+    );
+}
+
+
+export default Card;
